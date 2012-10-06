4f4af97974aa10098b0f7f53106b7d658f6b71b5
Ryan Barril <rbarril75@gmail.com>
2012-10-06 02:33:51 -0500

    Init.

---
+++ C/HelloWorld.c
@@ -0,0 +1,15 @@
+/*
+ *  HelloWorld.c
+ *  
+ *
+ *  Created by Ryan Barril on 9/30/12.
+ *  Copyright 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello World!\n");
+	return 0;
+}
---
+++ Java/HelloWorld.java
@@ -0,0 +1,13 @@
+//
+//  HelloWorld.java
+//  
+//
+//  Created by Ryan Barril on 9/30/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
